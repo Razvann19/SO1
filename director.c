f6eb45b3f845681f82e868d79adb0a50248838b8
Razvanel <constantin.baragan@student.upt.ro>
2024-04-26 09:29:43 +0300

    update fara close

--- director.c
+++ director.c
@@ -25,21 +25,20 @@ int pipe_cat_grep[2];
 int pipe_grep_parinte[2]; 
 
 if (pipe(pipe_cat_grep) <0 || pipe(pipe_grep_parinte) <0) {
-    printf("Eroare la crearea pipe-ului\n");
+    printf("Eroare la pipe\n");
     exit(1);
 }
 pid_t cat_pid = fork();
 
 if (cat_pid == -1) {
-    printf("Eroare la fork!!");
+    printf("Eroare la fork");
     exit(1);
 }
 
 if (cat_pid == 0) { 
     close(pipe_cat_grep[CITIRE]); 
     dup2(pipe_cat_grep[SCRIERE], 1); 
-    close(pipe_cat_grep[SCRIERE]);
-    
+
     execlp("cat", "cat", nume_fisier, NULL);
     printf("Eroare la execlp\n");
     exit(1);
@@ -54,11 +53,11 @@ if (grep_pid == -1) {
 if (grep_pid == 0) {
 close(pipe_cat_grep[SCRIERE]); 
 dup2(pipe_cat_grep[CITIRE], STDIN_FILENO); 
-close(pipe_cat_grep[CITIRE]); 
+
 
 close(pipe_grep_parinte[CITIRE]); 
 dup2(pipe_grep_parinte[SCRIERE], 1);
-close(pipe_grep_parinte[SCRIERE]); 
+
 
 execlp("grep", "grep", cuvant_cheie, NULL);
 printf("Eroare la exec\n");
@@ -79,7 +78,6 @@ while ((ceva = read(pipe_grep_parinte[CITIRE], buffer, sizeof(buffer))) > 0) {
 }
 
 close(pipe_grep_parinte[CITIRE]); 
-
 
 waitpid(cat_pid, NULL, 0);
 waitpid(grep_pid, NULL, 0);
